6db08f61c4392aa6e2f07e8de1c8532a1b1184dc
JoseTo21 <toro.jose.8e@gmail.com>
2023-08-04 05:04:21 -0400

    finish

--- src/controllers/course.controllers.js
+++ src/controllers/course.controllers.js
@@ -14,7 +14,7 @@ const create = catchError(async (req, res) => {
 
 const getOne = catchError(async (req, res) => {
   const { id } = req.params;
-  const result = await Course.findByPk(id);
+  const result = await Course.findByPk(id, { include: [Student] });
   if (!result) return res.sendStatus(404);
   return res.json(result);
 });
@@ -35,10 +35,24 @@ const update = catchError(async (req, res) => {
   return res.json(result[1][0]);
 });
 
+const setStundents = catchError(async (req, res) => {
+  //1-Se debe localizar al curso que le queremos agregar sus estudiantes, esto se hace mediante el metodo de sequelize findByPk
+  const { id } = req.params;
+  const course = await Course.findByPk(id);
+
+  //2-Setear los valores en nuestra instancia, esto se hace mediante el metodo de sequelize setNombre(req.body, ya que ahi se escriben nuestro estudiantes)
+  await course.setStudents(req.body);
+
+  //3-Leer nuestro cursos juntos nuestros estudiantes a travez del metodo getNombre()
+  const students = await course.getStudents();
+  return res.json(students);
+});
+
 module.exports = {
   getAll,
   create,
   getOne,
   remove,
   update,
+  setStundents,
 };
--- src/routes/course.router.js
+++ src/routes/course.router.js
@@ -4,6 +4,7 @@ const {
   getOne,
   remove,
   update,
+  setStundents,
 } = require("../controllers/course.controllers");
 const express = require("express");
 
@@ -12,5 +13,8 @@ const routerCourse = express.Router();
 routerCourse.route("/").get(getAll).post(create);
 
 routerCourse.route("/:id").get(getOne).delete(remove).put(update);
+
+// courses/:id/students
+routerCourse.route("/:id/students").post(setStundents);
 
 module.exports = routerCourse;
